f93deedf79b4efefc572b66410738ef01f06cb73
sigmatri <tatstrian@gmail.com>
2019-09-08 19:40:30 +0700

    sedah selesai

--- model/main.cpp
+++ model/main.cpp
@@ -1,18 +1,18 @@
-#include "PGM.hpp"
+#include "PPM.hpp"
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    PGM pgm("baboon.ascii.pgm");
+    PPM ppm("blackbuck.ascii.ppm");
     Image image;
-    pgm.readFile(image);
+    ppm.readFile(image);
 
     cout << image.width << endl;
     cout << image.height << endl;
     for(int i = 0; i<10; i++){
-        cout << (unsigned int) image.greyData[i] << endl;
+        cout << (unsigned int) image.colorData[i].R << "  " << (unsigned int) image.colorData[i].G << "  " << (unsigned int) image.colorData[i].B << endl;
     }
-    pgm.writeFile(image, "anjay");
+    ppm.writeFile(image, "anjay1");
     return 0;
 }
